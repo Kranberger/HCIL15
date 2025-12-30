4c6252dda103e70d4f6a33aa40e5ce6b890c6265
Kranberger <jochen.lichtenfels@online.de>
2025-12-30 14:39:21 +0100

    MyLogger V0.1

--- mylogger/Program.cs
+++ mylogger/Program.cs
@@ -6,6 +6,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("MyLogger V0.1");
+
         string[] portNames = SerialPort.GetPortNames();
         foreach (var portName in portNames)
         {
@@ -15,6 +17,7 @@ internal class Program
         List<byte[]> data = new();
         SerialPort serialPort = new("/dev/ttyACM0", 115200);
         serialPort.Open();
+        serialPort.ReadTimeout = 500;
         serialPort.Write("0");
         serialPort.Write("1");
         serialPort.Write("?");
@@ -28,12 +31,29 @@ internal class Program
                     var key = Console.ReadKey(true);
                     if (key.Key == ConsoleKey.Escape) // Taste zum Beenden (z.B. Escape)
                         break;
+
+                    if (key.KeyChar == '0' || key.KeyChar == '1' || key.KeyChar == '?')
+                    {
+                        serialPort.Write(key.KeyChar.ToString());
+                    }
                 }
 
                 byte[] buffer = new byte[256];
-                int r = serialPort.Read(buffer, 0, buffer.Length);
-                File.AppendAllBytes("output.bin", buffer[..r]);
-                Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, r));
+                try
+                {
+                    int r = serialPort.Read(buffer, 0, buffer.Length);
+                    File.AppendAllBytes("output2.bin", buffer[..r]);
+                    Console.Write(Encoding.ASCII.GetString(buffer, 0, r));
+                }
+                catch (TimeoutException)
+                {
+                    continue;
+                }   
+                catch (IOException ioex)
+                {
+                    Console.WriteLine($"IO Exception: {ioex.Message}");
+                    break;
+                }
             }
         }
         finally
